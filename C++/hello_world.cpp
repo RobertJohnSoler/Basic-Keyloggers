bd7fb7695865264f4a94da74624c6d735dcb280a
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-11-20 12:15:41 -0600

    hello world is working

---
+++ C++/hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+// int main(){
+//     cout << "Hello World";
+//     return 0;
+// }
+
+int main(){
+    printf("Hello World!!!");
+    return 0;
+}
---
+++ C++/key_logger.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+
+void main(){
+    
+}
