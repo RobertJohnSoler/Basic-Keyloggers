35eeefc97b116ede14e9aa5c25e49cfbeb0819da
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-11-20 20:31:49 -0600

    socket client-server POC done

---
+++ scratch_files/socket_server.py
@@ -0,0 +1,18 @@
+import socket
+
+s = socket.socket()
+print("Socket created.")
+
+port = 8080
+
+s.bind(('', port))
+print("Socket binded to ", port)
+
+s.listen(5)
+print("Socket is listening...")
+
+while True:
+    c, addr = s.accept()
+    print("Got connection from ", addr)
+    print(c.recv(1024, 0))
+    break
