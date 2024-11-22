3aa55e451f474dcbf413b488e9b23551a5863304
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-11-22 13:01:17 -0600

    put the whole socket server in a loop but needs fixing

--- scratch_files/socket_server.py
+++ scratch_files/socket_server.py
@@ -1,19 +1,27 @@
 import socket
 
-s = socket.socket()
-print("Socket created.")
-
-port = 8080
-
-s.bind(('', port))
-print("Socket binded to ", port)
-
-s.listen(5)
-print("Socket is listening...")
-
-
-c, addr = s.accept()
-print("Got connection from ", addr)
 while True:
-    print(c.recv(1024).decode(), end="", flush=True)
+
+    s = socket.socket()
+    print("Socket created.")
+
+    port = 8080
+
+    s.bind(('', port))
+    print("Socket binded to ", port)
+
+    s.listen(5)
+    print("Socket is listening...")
+
+
+    c, addr = s.accept()
+    print("Got connection from ", addr)
     
+    while True:
+        try:
+            print(c.recv(1024).decode(), end="", flush=True)
+        except:
+            print("Client must have disconnected.")
+            s.close()
+            break
+        
